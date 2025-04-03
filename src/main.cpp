ac9aaadde27ec671e7d9458802167b9df41eeebf
yuan_mo <2286087148@qq.com>
2025-04-03 16:57:23 +0800

    添加灯作为指示灯

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,38 @@
 #include <Arduino.h>
-#include "Event/ButtonState.h"
+#include "Event/ButtonState.hpp"
 #include "Button/EmmaButton.hpp"
+#include "SAM2695/SAM2695Synth.h"
+
+#define STATE_1_LED_TIME 800
+#define STATE_2_LED_TIME 400
+#define STATE_3_LED_TIME 200
 
 //创建音序器
-SAM2695Synth seq = SAM2695Synth::getInstance();
+SAM2695Synth synth = SAM2695Synth::getInstance();
 //创建状态机
 StateMachine stateMachine;
 //创建按钮
 EmmaButton button;
+StateManager* manager = StateManager::getInstance();
+
+
 int beatCount = 0;                // 打拍计数器
 unsigned long previousMillis = 0; // 记录上一次发送MIDI信号的时间
 int noteType = QUATER_NOTE;                 // 音符类型选择，0（四分音符）、1（八分音符）、2（十六音符）
 int beatsPerBar = BEATS_BAR_DEFAULT;              // 每小节拍数，可以是2、3或4
 bool isPressed = false;
-
-void multiTrackPlay()
-{
-    unsigned long interval = (BASIC_TIME / seq.getBpm()) / (noteType + 1);
-    unsigned long currentMillis = millis();
-    if (currentMillis - previousMillis >= interval)
-    {
-        previousMillis = currentMillis;
-        if(channel_1_on_off_flag)
-        {
-            seq.play(CHANNEL_0);
-        }
-        if(channel_2_on_off_flag)
-        {
-            seq.play(CHANNEL_1);
-        }
-        if(channel_3_on_off_flag)
-        {
-            seq.play(CHANNEL_2);
-        }
-        if(channel_4_on_off_flag)
-        {
-            seq.play(CHANNEL_3);
-        }
-        // if(drum_on_off_flag)
-        // {
-        //     seq.drumPlay();
-        // }
-    }
-}
+uint8_t drupCount = 0;
+uint8_t voice = 50;
+uint8_t  modeID = 0;//模式ID
+int ledTime = 0;//LED反转时间，500ms
+unsigned long previousMillis2 = 0;//记录上一次灯的时间
 
 // 获取下一个输入事件
 Event* getNextEvent()
 {
-    //用来控制长按和短按事件，松开的时候才会触发短按，否则是长按
+    //Used to control long press and short press events.
+    //A short press is triggered when released, otherwise
+    //it is considered a long press.
     if(button.A.pressed()==BtnAct::Pressed
         || button.B.pressed()==BtnAct::Pressed
         || button.C.pressed()==BtnAct::Pressed
@@ -59,7 +44,6 @@ Event* getNextEvent()
     if(button.A.longPressed()==BtnAct::LongPressed)
     {
         isPressed = false;
-        Serial.println("Button A long pressed");
         Event* e = new Event(EventType::BtnALongPressed);
         return e;
     }
@@ -67,7 +51,6 @@ Event* getNextEvent()
     if(button.B.longPressed()==BtnAct::LongPressed)
     {
         isPressed = false;
-        Serial.println("Button B long pressed");
         Event* e = new Event(EventType::BtnBLongPressed);
         return e;
     }
@@ -75,7 +58,6 @@ Event* getNextEvent()
     if(button.C.longPressed()==BtnAct::LongPressed)
     {
         isPressed = false;
-        Serial.println("Button C long pressed");
         Event* e = new Event(EventType::BtnCLongPressed);
         return e;
     }
@@ -83,7 +65,6 @@ Event* getNextEvent()
     if(button.D.longPressed()==BtnAct::LongPressed)
     {
         isPressed = false;
-        Serial.println("Button D long pressed");
         Event* e = new Event(EventType::BtnDLongPressed);
         return e;
     }
@@ -93,7 +74,6 @@ Event* getNextEvent()
         if(isPressed)
         {
             isPressed = false;
-            Serial.println("Button A pressed");
             Event* e = new Event(EventType::BtnAPressed);
             return e;
         }
@@ -103,7 +83,6 @@ Event* getNextEvent()
         if(isPressed)
         {
             isPressed = false;
-            Serial.println("Button B pressed");
             Event* e = new Event(EventType::BtnBPressed);
             return e;
         }
@@ -113,7 +92,6 @@ Event* getNextEvent()
         if(isPressed)
         {
             isPressed = false;
-            Serial.println("Button C pressed");
             Event* e = new Event(EventType::BtnCPressed);
             return e;
         }
@@ -123,7 +101,6 @@ Event* getNextEvent()
         if(isPressed)
         {
             isPressed = false;
-            Serial.println("Button D pressed");
             Event* e = new Event(EventType::BtnDPressed);
             return e;
         }
@@ -131,14 +108,87 @@ Event* getNextEvent()
 
     return nullptr;
 }
+// 多轨播放
+void multiTrackPlay()
+{
+    unsigned long interval = (BASIC_TIME / synth.getBpm()) / (noteType + 1);
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval)
+    {
+        previousMillis = currentMillis;
+        if(channel_1_on_off_flag)
+        {
+            uint8_t pitch = synth.getPitch();
+            synth.setNoteOn(CHANNEL_0,synth.getPitch(),voice);
+        }
+        if(channel_2_on_off_flag)
+        {
+            synth.setNoteOn(CHANNEL_1,synth.getPitch(),voice);
+        }
+        if(channel_3_on_off_flag)
+        {
+            synth.setNoteOn(CHANNEL_2,synth.getPitch(),voice);
+        }
+        if(channel_4_on_off_flag)
+        {
+            synth.setNoteOn(CHANNEL_3,synth.getPitch(),voice);
+        }
+        if(drum_on_off_flag)
+        {
+            if(drupCount % 4 == 0)
+            {
+                synth.setNoteOn(CHANNEL_9,NOTE_C2,voice);
+                synth.setNoteOn(CHANNEL_9,NOTE_FS2,voice);
+            }
+            else if(drupCount % 4 == 1)
+            {
+                synth.setNoteOn(CHANNEL_9,NOTE_FS2,voice);
+            }
+            else if(drupCount % 4 == 2)
+            {
+                synth.setNoteOn(CHANNEL_9,NOTE_D2,voice);
+                synth.setNoteOn(CHANNEL_9,NOTE_FS2,voice);
+            }
+            else if(drupCount % 4 == 3)
+            {
+                synth.setNoteOn(CHANNEL_9,NOTE_FS2,voice);
+            }
+            drupCount++;
+        }
+    }
+}
+//灯光显示
+void ledShow()
+{
+    modeID = stateMachine.getCurrentState()->getID();
+    if(modeID == ButtonState1::ID)
+    {
+        ledTime = STATE_1_LED_TIME;
+    }
+    else if(modeID == ButtonState2::ID)
+    {
+        ledTime = STATE_2_LED_TIME;
+    }
+    else if(modeID == ButtonState3::ID)
+    {
+        ledTime = STATE_3_LED_TIME;
+    }
+    unsigned long currentMillis = millis();
+    if(currentMillis - previousMillis2 >= ledTime)
+    {
+        previousMillis2 = millis();
+        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    }
+}
 
 void setup() {
+    //初始化串口
     Serial.begin(115200);
+    //初始化LED
+    pinMode(LED_BUILTIN, OUTPUT);
     delay(3000);
     //初始化音序器
-    seq.begin();
-    //注册状态
-    StateManager* manager = StateManager::getInstance();
+    synth.begin();
     //注册按钮状态
     manager->registerState(new ButtonState1());
     manager->registerState(new ButtonState2());
@@ -164,7 +214,7 @@ void loop() {
         delete event;
     }
     multiTrackPlay();
+    ledShow();
 }
-
 
 
